9198130954b83650cef58758ead2ff07931ab1f9
Megha <meghaw42@gmail.com>
2022-12-26 20:12:43 +0530

    updates

--- setup.py
+++ setup.py
@@ -4,11 +4,17 @@ from typing import List
 
 REQUIREMENT_FILE_NAME = "requirements.txt"
 HYPHEN_E_DOT = "-e ."
-def get_requiremetns()->List[str]:...
+
+def get_requiremetns()->List[str]:
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        requirement_list = requirement_file.readlines()
+        requirement_list = [requirements_name.replace("\n"," ") for requirement_name in requirement_list]
+        if HYPHEN_E_DOT in requirement_list:
+            requirement_list.remove(HYPHEN_E_DOT)
+        return requirements_list
 
 
-def get_requirements():
-    pass
+
 
 setup(
     name = 'sensor'
